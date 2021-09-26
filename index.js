cef2759f21d28fc328d149f556eb659bc88272ff
Tiffany_Tam <tamtiffany89@gmail.com>
2021-09-26 17:23:57 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function introduction(name= "Josh"){
+    return`Hi, my name is ${name}.`
+}
+function introductionWithLanguage(name ="Josh", language = "Javascript") {
+    return`Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+function introductionWithLanguageOptional(name , language = "JavaScript") {
+    return`Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+// function introductionWithLanguageOptional(name ="Josh", language = "Javascript") {
+//     return`Hi, my name is ${name} and I am learning to program in ${language}.`;
+// }
+
